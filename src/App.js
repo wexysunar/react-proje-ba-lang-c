e635fbd8f695760c33476041bc998bb94a7ba0ff
wexysunar <yusufmiracgoksoy@gmail.com>
2023-11-14 18:07:35 +0300

    ilk react projem

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg"
+
+function App() {
+  return (
+    <div className="App">
+      <img src="/logo512.png"/>
+      <img src={logo}/>
+    </div>
+  );
+}
+
+export default App;
